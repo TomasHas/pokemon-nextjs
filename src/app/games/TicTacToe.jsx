4cc77e877105bc170dac9ba5e13658caafbf2b36
Tomas Hastrup <thastrup@gmail.com>
2024-06-14 16:10:12 -0300

    games3

--- src/app/games/TicTacToe.jsx
+++ src/app/games/TicTacToe.jsx
@@ -1,9 +1,9 @@
 "use client";
 import { TicTacToeCalculator } from "../lib/utils";
 import TicTacToeSquare from "./TicTacToeSquare";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { Forum } from "next/font/google";
+import Loading from "./loading";
 
 export default function TicTacToe() {
   const searchParams = useSearchParams();
@@ -183,76 +183,78 @@ export default function TicTacToe() {
   };
   console.log(squares);
   return (
-    <div className="flex flex-row justify-around">
-      {" "}
-      <div
-        className={
-          activePlayer === "player_one"
-            ? " h-10 w-28 text-center  bg-yellow-500"
-            : "h-10 w-28 text-center  bg-gray-500"
-        }
-      >
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-row justify-around">
         {" "}
-        <p>player one</p>
+        <div
+          className={
+            activePlayer === "player_one"
+              ? " h-10 w-28 text-center  bg-yellow-500"
+              : "h-10 w-28 text-center  bg-gray-500"
+          }
+        >
+          {" "}
+          <p>player one</p>
+        </div>
+        <div className="grid grid-cols-3 gap-3 ">
+          <TicTacToeSquare
+            name="one"
+            value={squares.one.character}
+            handleClick={handleClick}
+            currentPlayer={activePlayer}
+          />{" "}
+          <TicTacToeSquare
+            name="two"
+            value={squares.two.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="three"
+            value={squares.three.character}
+            handleClick={handleClick}
+          />
+          <TicTacToeSquare
+            name="four"
+            value={squares.four.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="five"
+            value={squares.five.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="six"
+            value={squares.six.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="seven"
+            value={squares.seven.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="eight"
+            value={squares.eight.character}
+            handleClick={handleClick}
+          />{" "}
+          <TicTacToeSquare
+            name="nine"
+            value={squares.nine.character}
+            handleClick={handleClick}
+          />
+        </div>{" "}
+        <div
+          className={
+            activePlayer === "player_two"
+              ? " h-10 w-28 text-center  bg-yellow-500"
+              : "h-10 w-28 text-center  bg-gray-500"
+          }
+        >
+          <p>player two</p>
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-3 ">
-        <TicTacToeSquare
-          name="one"
-          value={squares.one.character}
-          handleClick={handleClick}
-          currentPlayer={activePlayer}
-        />{" "}
-        <TicTacToeSquare
-          name="two"
-          value={squares.two.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="three"
-          value={squares.three.character}
-          handleClick={handleClick}
-        />
-        <TicTacToeSquare
-          name="four"
-          value={squares.four.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="five"
-          value={squares.five.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="six"
-          value={squares.six.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="seven"
-          value={squares.seven.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="eight"
-          value={squares.eight.character}
-          handleClick={handleClick}
-        />{" "}
-        <TicTacToeSquare
-          name="nine"
-          value={squares.nine.character}
-          handleClick={handleClick}
-        />
-      </div>{" "}
-      <div
-        className={
-          activePlayer === "player_two"
-            ? " h-10 w-28 text-center  bg-yellow-500"
-            : "h-10 w-28 text-center  bg-gray-500"
-        }
-      >
-        <p>player two</p>
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
